9737473863ab33fffb7330b91b1b7fcb70103494
Qbuiba <37890918+Qbuiba@users.noreply.github.com>
2024-04-28 23:24:22 -0500

    update to spring boot app

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,16 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  */
+
+@SpringBootApplication
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
-
-    public String getMessage() {
-        return MESSAGE;
+        SpringApplication.run(App.class, args);
     }
 }
